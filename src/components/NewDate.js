cd3f7a415a51e81b31f993c853aab272e51ef95f
lyndsiWilliams <kcatgirl@gmail.com>
2019-11-18 13:33:31 -0600

    Created basic food entry form at FoodEntry.js

--- src/components/NewDate.js
+++ src/components/NewDate.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-const Footer = props => {
-    console.log(props);
+export default function NewDate() {
+    const [date, setDate] = useState([]);
 
-    return (
-        <div>
-            <button onClick={() => alert(props.list.service_version)}>Server Version</button> {/* ser_ver */}
-            <button onClick={() => alert("HD/SD switch!")}>Want to see it in HD?</button> {/* switch src between url and hdurl */}
-            <button onClick={() => alert(props.list.media_type)}>Media Type</button> {/* med_type */}
-        </div>
-    );
-};
+    useEffect(() => {
+        
+    }, [])
+}
 
-export default Footer;
+// const Footer = props => {
+//     console.log(props);
+
+//     return (
+//         <div>
+//             <button onClick={() => alert(props.list.service_version)}>Server Version</button> {/* ser_ver */}
+//             <button onClick={() => alert("HD/SD switch!")}>Want to see it in HD?</button> {/* switch src between url and hdurl */}
+//             <button onClick={() => alert(props.list.media_type)}>Media Type</button> {/* med_type */}
+//         </div>
+//     );
+// };
+
+// export default Footer;
